757240e7bbf4fb6e3d41ee168654e278e7cb5801
lindseygordon <lindseygordon@LINDSEYGORDD095>
2024-02-01 13:55:39 -0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _04_Practice_SourceControl.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
